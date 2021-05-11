7ae557469ddc07926dda6065946ba1639c7650a1
id18oll <lyxelloskar@gmail.com>
2021-05-11 12:00:48 +0200

    HEHE xD

---
+++ src/components/TransactionHistory.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TransactionHistory extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
