fb7d45f6ffae401da79c2f8a0d30a25d619f850b
Håkan <lindahl9898@gmail.com>
2021-05-11 11:51:45 +0200

    Added initial axios call

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import './App.css';
 import {Helmet} from "react-helmet";
 
 function App() {
+
+  const api = (event) => {    
+    const axios = require('axios');
+    const params = {
+      access_key: 'da50ae70b9dd9a40022f6cc5ef93f4e5'
+    }
+
+    axios.get('http://api.marketstack.com/v1/tickers', {params})
+      .then(response => {
+        const apiResponse = response.data;
+        console.log(apiResponse); 
+      }).catch(error => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="App">
       <Helmet>
@@ -9,7 +25,7 @@ function App() {
         <title>StockNoob</title>
         <link rel="canonical" href="http://mysite.com/example" />
       </Helmet>
-      <header className="App-header">
+      <header className="App-header" onClick={api}>
         Hello
       </header>
     </div>
