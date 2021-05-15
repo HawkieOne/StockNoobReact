278c10ce6e85c0c01a0f279ff44366380d75a02a
Håkan <lindahl9898@gmail.com>
2021-05-16 00:32:40 +0200

    Added a test method for Authorization on api

--- src/App.js
+++ src/App.js
@@ -12,11 +12,42 @@ function App() {
     axios.get('http://api.marketstack.com/v1/tickers', {params})
       .then(response => {
         const apiResponse = response.data;
-        console.log(apiResponse); 
+        console.log(apiResponse);
+        apiResponse.data.map((stock, index) =>                     
+            axios.post('https://localhost:44388/api/stock/addStock',
+              {
+                "Stock_Name": stock.name,
+                "Stock_Shortening": stock.symbol  
+              }             
+            )
+            .then(
+              console.log(stock.name + " added")
+            ).catch(error =>
+              console.log(stock.name + " was not added")
+            )
+         );
       }).catch(error => {
         console.log(error);
       });
   };
+
+
+  const test = (event) => {    
+    var axios = require("axios").default;
+
+    var options = {
+      method: 'GET',
+      url: 'http://localhost:3010/api/private',
+      // headers: {authorization: 'Bearer YOUR_ACCESS_TOKEN'}
+    };
+
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
+  };
+
 
   return (
     <div className="App">
@@ -25,7 +56,7 @@ function App() {
         <title>StockNoob</title>
         <link rel="canonical" href="http://mysite.com/example" />
       </Helmet>
-      <header className="App-header" onClick={api}>
+      <header className="App-header" onClick={test}>
         Hello
       </header>
     </div>
