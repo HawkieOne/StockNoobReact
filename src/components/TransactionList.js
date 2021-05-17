21a9d7141f7a453731dd7982efb6d2689cbc077e
Reza Parvar <id18rpr@cs.umu.se>
2021-05-17 12:00:59 +0200

    Added Stock and transaction page, almost done

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import TranscationItem from  './TranscationItem';
+import './TransactionList.css'
 
 
 function TransactionList() {
@@ -9,13 +10,38 @@ function TransactionList() {
         {shares: "7", name: "Tesla", date:"2021-01-19"},
         {shares: "8", name: "Amazon", date:"2021-05-09"},
         {shares: "9", name: "Google", date:"2021-11-19"},
-        {shares: "6", name: "BNW", date:"2021-12-02"}
+        {shares: "6", name: "BNW", date:"2021-12-02"},
+        {shares: "16", name: "Redbull", date:"2021-03-02"}
     ]
     return (
         <>
             <div>
                 <div>
                     <h2 className="mt-5 text-warning">Transactions</h2>
+                    <div className="w-75 meny"> 
+                        <div className="filterBy">
+                            <p>Filter: </p>
+                            <label class="container">Buy
+                                <input type="radio" name="radio"/>
+                                <span class="checkmark"></span>
+                            </label>
+                            <label class="container">Sell
+                                <input type="radio" name="radio"/>
+                                <span class="checkmark"></span>
+                            </label>
+                        </div>
+                        <div className="sortedBy">
+                            <p>Sort: </p>
+                            <label class="container">Newest
+                                <input type="radio" name="radio"/>
+                                <span class="checkmark"></span>
+                            </label>
+                            <label class="container">Oldest
+                                <input type="radio" name="radio"/>
+                                <span class="checkmark"></span>
+                            </label>
+                        </div>
+                    </div>
                 </div>
                 <hr className="w-75 hrTag"></hr>
                 {transactions.map((transactions, i) => (
