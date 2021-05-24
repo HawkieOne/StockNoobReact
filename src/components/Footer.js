2520f51993ac56b1e025f782de0deb2181f96b84
Håkan <lindahl9898@gmail.com>
2021-05-24 17:14:19 +0200

    WIP Make the footer smaller

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,30 +7,28 @@ import {faFacebookF, faTwitter, faInstagram} from "@fortawesome/free-brands-svg-
 
 export default function Footer() {
     return (
-        <div className="Bottom">
-            <div className="container">
-            <div className="left">
-                CONTACT
-                <hr className="seperator"/>
-                <span>
-                <FontAwesomeIcon className="mail" icon={faAddressBook} />
-                Stocknoob@stocks.com
-                </span>
-            </div>
-            <div className="middle">
-                ABOUT
-                <hr className="seperator"/>
-                <span>
-                Stocknoob is a fictional training 
-                <br/>
-                application designed as a learning tool</span>
-            </div>
-            <div className="right">
-                <FontAwesomeIcon className="icon" icon={faInstagram} />
-                <FontAwesomeIcon className="icon" icon={faTwitter} />
-                <FontAwesomeIcon className="icon" icon={faFacebookF} />
-            </div>
-            </div>
+        <div className="footer row">
+            <div className="col">
+                    <h5>CONTACT</h5>
+                    <hr className="seperator"/>
+                    <span>
+                    <FontAwesomeIcon className="mail" icon={faAddressBook} />
+                    Stocknoob@stocks.com
+                    </span>
+                </div>
+                <div className="col">                    
+                    <h5>ABOUT</h5>
+                    <hr className="seperator"/>
+                    <span>
+                    Stocknoob is a fictional training 
+                    <br/>
+                    application designed as a learning tool</span>
+                </div>
+                <div className="col row">
+                    <FontAwesomeIcon className="icon col" icon={faInstagram} />
+                    <FontAwesomeIcon className="icon col" icon={faTwitter} />
+                    <FontAwesomeIcon className="icon col" icon={faFacebookF} />
+                </div>
         </div>
     )
 }
