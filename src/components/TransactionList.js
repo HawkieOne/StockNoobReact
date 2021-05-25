2f344df2060c6e8ec804700a7fdd80e7ca6f3244
HawkieOne <Pr0gr@mm3r!ng2019>
2021-05-25 20:33:46 +0200

    Transaction list works against the user

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,59 +1,102 @@
 import React, {useState, useEffect} from 'react'
 import TranscationItem from  './TranscationItem';
 import './TransactionList.css'
+import {  faArrowLeft} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import history from './History'
 
 
 export default function TransactionList(prop) {
+
+    var [user, setUser] = useState(null);
+    var [transactions, setTransactions] = useState(null);
+    var [loaded, setLoaded] = useState(false);
 
     useEffect(() => {
         const {user} = prop.location.state;
         console.log("TRANSACTIONS");
         console.log(user);
-        // setSearchVal(search);
-    }, []);
+        setUser(user)},[]);
 
-    const transactions = [
-        {shares: "12", name: "Facebook", date:"2021-03-19"},
-        {shares: "7", name: "Tesla", date:"2021-01-19"},
-        {shares: "8", name: "Amazon", date:"2021-05-09"},
-        {shares: "9", name: "Google", date:"2021-11-19"},
-        {shares: "6", name: "BNW", date:"2021-12-02"},
-        {shares: "16", name: "Redbull", date:"2021-03-02"}
-    ]
+    useEffect(() => {  
+        if (user !== null) {
+            const axios = require('axios');       
+            var options = {
+            method: 'PUT',
+            url: `https://stocknoob.azurewebsites.net/stock/transactions/${user.UserID}`,
+            headers: { authorization: 'Bearer ' + user.Token},
+            data: 2
+            };
+            
+            let array = [];
+
+            axios.request(options).then(function (response) {
+            const apiResponse = response.data;
+            console.log(apiResponse);
+            apiResponse.map((trsc, index) => {
+                array.push(trsc);
+            });
+            setTransactions(array);
+            }).catch(function (error) {
+            console.error(error);
+            });
+        }}, [user]);
+
+    useEffect(() => {  
+        if (transactions !== null) {
+            setLoaded(true);
+        }}, [transactions]);
+
+    const goBack = () => {    
+        const path = "/Overview";
+            history.push({
+            pathname: path,
+            state: { 
+                user: user
+            }
+        })  
+    };
+
     return (
         <>
-            <div>
+            <div className="mb-5">
                 <div>
-                    <h2 className="mt-5 text-warning">Transactions</h2>
-                    <div className="w-75 meny"> 
-                        <div className="filterBy">
-                            <p>Filter: </p>
-                            <label class="container">Buy
-                                <input type="radio" name="radioBuy"/>
-                                <span class="checkmark"></span>
-                            </label>
-                            <label class="container">Sell
-                                <input type="radio" name="radioSell"/>
-                                <span class="checkmark"></span>
-                            </label>
-                        </div>
-                        <div className="sortedBy">
-                            <p>Sort: </p>
-                            <label class="container">Newest
-                                <input type="radio" name="radioNew"/>
-                                <span class="checkmark"></span>
-                            </label>
-                            <label class="container">Oldest
-                                <input type="radio" name="radioOld"/>
-                                <span class="checkmark"></span>
-                            </label>
-                        </div>
+                    <div className="position-relative">
+                        <h2 className="mt-5 text-warning">Transactions</h2>
+                        <FontAwesomeIcon className="icon fa-lg yellow backArrow" icon={faArrowLeft} onClick={goBack}/>
+                    </div>                    
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/>
+                        <label class="form-check-label" for="inlineRadio1">Buy</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
+                        <label class="form-check-label" for="inlineRadio2">Sell</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3"/>
+                        <label class="form-check-label" for="inlineRadio3">Old</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio4" value="option4"/>
+                        <label class="form-check-label" for="inlineRadio4">New</label>
                     </div>
                 </div>
                 <hr className="w-75 hrTag"></hr>
-                {transactions.map((transactions, i) => (
-                    <TranscationItem shares={transactions.shares} name={transactions.name} date={transactions.date}/>
-                ))}
+                {loaded ?
+                    transactions.map((trsc, i) => (
+                        <TranscationItem 
+                            key={i} 
+                            shares={trsc.Trsc_Amount} 
+                            price={trsc.Trsc_Price} 
+                            date={trsc.Trsc_Date} 
+                            type={trsc.Trsc_Type}
+                            name={trsc.Trsc_Name}
+                        />
+                    ))
+                    :
+                    <h1 className="yellow">Loading...</h1>
+                }
             </div>
         </> 
     )
--- src/components/TranscationItem.js
+++ src/components/TranscationItem.js
@@ -7,14 +7,31 @@ function TranscationItem(props) {
     return (
         <>
             <div>
-                <div className="w-75 transc_Item">
-                    <div className="Tr_item">
-                        <div className="Tr_itemShare">
-                            <FaArrowUp size="2.5em" color="green"/> 
-                            <p>{props.shares} Shares</p>
+                <div className="w-75 trscItem">
+                    <div className="row">
+                        <div className="col Tr_itemShare">
+                            {props.type ===  'BUY' ?
+                                <FaArrowUp size="2.5em" color="green"/> 
+                                :
+                                <FaArrowDown size="2.5em" color="red"/> 
+                            }                                                        
                         </div>
-                        <p>{props.name}</p>
-                        <p>{props.date}</p>
+                        <div className="col-4 d-flex">
+                            <p className="yellow">Name: </p>
+                            <p className="ml-3">{props.name}</p>
+                        </div>      
+                        <div className="col d-flex">
+                            <p className="yellow">Shares: </p>
+                            <p className="ml-3">{props.shares}</p>
+                        </div>        
+                        <div className="col d-flex">
+                            <p className="yellow">Price: </p>
+                            <p className="ml-3">{props.price}</p>
+                        </div>
+                        <div className="col d-flex">
+                            <p className="yellow">Date: </p>
+                            <p className="ml-3">{new Date(props.date).toLocaleDateString()}</p>
+                        </div>
                     </div>
                 </div>  
             </div>
